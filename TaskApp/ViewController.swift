967345167491c139c80aac8c7f6aa82c047823fb
Anoop Dhiman <anoopdhiman@anoops-macbook-pro.local>
2016-09-11 11:27:14 +0530

    Valid Email check

--- TaskApp/ViewController.swift
+++ TaskApp/ViewController.swift
@@ -25,18 +25,49 @@ class ViewController: UIViewController {
         
         if(segmentFlag){
             btnLogin.setTitle("Login", forState: btnLogin.state)
-            segmentFlag = false
+            segmentFlag = false            
         }
         else{
             btnLogin.setTitle("Signup", forState: btnLogin.state)
             segmentFlag = true
+            txtEmail.focused
         }
     }
     
     @IBAction func loginAction(sender: AnyObject) {
+        
+        let currentTitle = btnLogin.currentTitle
+        
+        if(currentTitle == "Signup"){
+            //do signup
+            
+            if(checkValidEmail(txtEmail.text!)){
+                //do firebase signup                
+            }
+            else
+            {
+                showAlert("Alert", message: "Invalid Email", closetitle: "Try again")
+            }
+            
+            
+            
+        }
+        else if(currentTitle == "Login"){
+            //do login
+        }
+        else{
+            showAlert("Alert !!!", message: "Somthing went wrong", closetitle: "Try again leter")
+            
+        }
+        
+        
+        
+        
+        
     }
         override func viewDidLoad() {
         super.viewDidLoad()
+        btnLogin.layer.cornerRadius = 5
             
         //view.backgroundColor:UIColor(patternImage: UIImage[imageNamed:"@wallpaper.png"])
         
@@ -48,6 +79,21 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //check valid email
+    func checkValidEmail(email:String) -> Bool {
+        return email.rangeOfString("^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$", options: .RegularExpressionSearch) != nil
+    }
+    
+    
+    //Show Alert Message
+    func showAlert (title:String,message:String, closetitle:String)  {
+        let alertController = UIAlertController(title: title, message:
+            message, preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: closetitle, style: UIAlertActionStyle.Default,handler: nil))
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
 
 }
