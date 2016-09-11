531865409a2f33a7d7f83e17530f245662414651
Anoop Dhiman <anoopdhiman@anoops-macbook-pro.local>
2016-09-11 23:06:00 +0530

    Fire signup up Succeed

--- TaskApp/AppDelegate.swift
+++ TaskApp/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+                //FIRApp.configure()
+        
+        
+        
+        
+        
+        
         // Override point for customization after application launch.
         return true
     }
--- TaskApp/ViewController.swift
+++ TaskApp/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseAuth
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -20,7 +21,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPwd: UITextField!
     
-    
     //Segment Control Action
     @IBAction func segmentAction(sender: AnyObject) {
         if(segmentFlag){
@@ -32,15 +32,29 @@ class ViewController: UIViewController {
             segmentFlag = true
             txtEmail.focused
         }
-    }
-    
+    }    
     //Signup/ Login Acton
     @IBAction func loginAction(sender: AnyObject) {
         let currentTitle = btnLogin.currentTitle
         if(currentTitle == "Signup"){
             //do signup
-            if(checkValidEmail(txtEmail.text!)){
-                //do firebase signup                
+            if(checkValidEmail(txtEmail.text!) && (txtPwd.text != "")){
+                //do firebase signup
+                
+                FIRAuth.auth()?.createUserWithEmail((txtEmail.text)!, password: (txtPwd.text)!, completion: { (let user, let error) in
+                    if let cUser = FIRAuth.auth()?.currentUser{
+                        let uid = cUser.uid
+                        let uDefaults = NSUserDefaults.standardUserDefaults()
+                        uDefaults.setObject(uid, forKey: "uid")
+                        uDefaults.synchronize()
+                        self.showAlert("Success", message: "Registered Successfully", closetitle: "Click to Login")
+                        self.txtEmail.text = ""
+                        self.txtPwd.text = ""
+                        self.btnSegment.selectedSegmentIndex = 0
+                        self.btnLogin.setTitle("Login", forState: self.btnLogin.state)
+                    }
+                    //print(user)
+                })
             }
             else{
                 showAlert("Alert", message: "Invalid Email", closetitle: "Try again")
@@ -57,15 +71,20 @@ class ViewController: UIViewController {
         override func viewDidLoad() {
         super.viewDidLoad()
         btnLogin.layer.cornerRadius = 5
+        FIRApp.configure()
             
-        //view.backgroundColor:UIColor(patternImage: UIImage[imageNamed:"@wallpaper.png"])
+        //self.view.backgroundColor = UIColor(patternImage: <#T##UIImage#>(named:"wallpaper.jpg"))
+          
+            self.view.backgroundColor = UIColor(patternImage: UIImage(named: "wallpaper.jpg")!)
+            
+            
+       
          }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     
     //check valid email
     func checkValidEmail(email:String) -> Bool {
